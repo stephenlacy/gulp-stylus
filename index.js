2a8b7bc4af4826e35e18a55fd5c3f6aea6072b7b
Eric Schoffstall <contra@wearefractal.com>
2013-12-04 13:26:23 -0700

    be more gulp-like

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 var stream = require('event-stream');
-var clone = require('clone');
 var stylus = require('stylus');
 var ext = require('gulp-util').replaceExtension;
 
 module.exports = function (options) {
-  var opts = options ? clone(options) : {};
+  var opts = options ? options : {};
 
   function stylusstream (file, cb) {
     // file is on object passed in by gulp
     // file.contents is always a Buffer
     
-    var cloned = clone(file);
-
-    opts.data = cloned.contents.toString();
+    opts.data = String(file.contents);
 
     opts.error = function (err) {
       cb(err);
     };
 
     stylus.render(opts.data, function(err, css){
+      if (err) return cb(err);
 
-      cloned.path      = ext(cloned.path, '.css');
-      cloned.shortened = cloned.shortened && ext(cloned.shortened, '.css');
-      cloned.contents  = new Buffer(css);
+      file.path      = ext(file.path, '.css');
+      file.shortened = file.shortened && ext(file.shortened, '.css');
+      file.contents  = new Buffer(css);
 
-      cb(null, cloned);
+      cb(null, file);
     });
   }
 
