55480b8d1fa54c5cac7228972bbf42f397e662a8
Steve <me@slacy.me>
2013-12-04 13:09:17 -0700

    inital commit

---
+++ examples/Gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var stylus = require('../');
+
+
+// Get one .styl file and render
+gulp.task('one', function () {
+	gulp.src('./css/one.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('./css'));
+});
+
+
+// Get all .styl files in one folder and render
+gulp.task('one', function () {
+	gulp.src('./css/blue/*.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('./css/blue'));
+});
+
+
+
+// Get and render recursive stylus files
+gulp.task('stylus', function () {
+	gulp.src('./css/**/*.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('./css'));
+});
+
+
+
+
+
+// Default gulp task to run
+gulp.task('default', function(){
+	gulp.run('stylus', 'one');
+});
