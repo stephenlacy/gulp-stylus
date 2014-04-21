13d37d0bb09deb37cda11cec636387486243100a
Steve Lacy <me@slacy.me>
2014-04-21 13:45:00 -0700

    through2 + error reporting

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-var map = require('map-stream');
+var through = require('through2');
 var stylus = require('accord').load('stylus');
+var gutil = require("gulp-util");
 var rext = require('replace-ext');
 var path = require('path');
 
 module.exports = function (options) {
   var opts = options ? options : {};
 
-  function stylusstream (file, cb) {
+  return through.obj(function (file, enc, cb) {
 
-    if (file.isNull()) return cb(null, file); // pass along
-    if (file.isStream()) return cb(new Error("gulp-stylus: Streaming not supported"));
-    if (path.extname(file.path) === '.css') return cb(null, file);
+    if (file.isStream()) return cb(new gutil.PluginError("gulp-stylus: Streaming not supported"));
+    if (file.isNull()){
+      this.push(file);
+      return cb();
+    }
+    if (path.extname(file.path) === '.css'){
+      this.push(file);
+      return cb();
+    }
+    if (!opts.filename) opts.filename = file.path;
 
-    if (!opts.filename)
-      opts.filename = file.path;
-
+    that = this;
     stylus.render(file.contents.toString('utf8'), opts)
     .catch(function(err){
-      if(err) cb(new Error(err));
+      if(err){
+        if(opts.errors) gutil.log('gulp-stylus', gutil.colors.cyan(err));
+        that.emit('error', new gutil.PluginError('gulp-stylus', err));
+      }
     })
     .then(function(css){
       file.path = rext(file.path, '.css');
       file.contents = new Buffer(css);
-      cb(null, file);
+      that.push(file);
+      cb();
     });
+  });
 
-  }
-
-  return map(stylusstream);
 };
