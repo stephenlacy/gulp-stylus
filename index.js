de5c87301c906d45286a40c344ec117dd9d3e331
Steve Lacy <me@slacy.me>
2014-07-11 14:06:59 -0700

    Clean plugin

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-var through = require('through2');
-var stylus = require('accord').load('stylus');
-var gutil = require("gulp-util");
-var rext = require('replace-ext');
 var path = require('path');
+var gutil = require('gulp-util');
+var through = require('through2');
+var rext = require('replace-ext');
+var clone = require('lodash.clone');
+var stylus = require('accord').load('stylus');
 
-module.exports = function (options) {
-  var opts = options ? options : {};
-  var paths = opts.paths ? opts.paths : [];
+
+module.exports = function (opts) {
 
   return through.obj(function (file, enc, cb) {
 
-    if (file.isStream()) return cb(new gutil.PluginError("gulp-stylus: Streaming not supported"));
+    if (file.isStream()) {
+      return cb(new gutil.PluginError('gulp-stylus: Streaming not supported'));
+    }
     if (file.isNull()){
-      this.push(file);
-      return cb();
+      return cb(null, file);
     }
-    if (path.extname(file.path) === '.css'){
-      this.push(file);
-      return cb();
+    if (path.extname(file.path) !== '.styl'){
+      return cb(null, file);
     }
-    opts.filename = file.path;
-    if (!opts.paths) opts.paths = paths.concat([path.dirname(file.path)]);
+    var opt = opts ? clone(opts) : {};
+    opt.filename = file.path;
 
-    that = this;
-    stylus.render(file.contents.toString('utf8'), opts)
+    // scope file to its own folder
+    // if no paths given
+    if (!opt.paths) {
+      opt.paths = [];
+    }
+    opt.paths.push(path.dirname(file.path));
+
+    stylus.render(file.contents.toString('utf8'), opt)
     .catch(function(err){
-      if(err){
-        if(opts.errors) gutil.log('gulp-stylus', gutil.colors.cyan(err));
-        that.emit('error', new gutil.PluginError('gulp-stylus', err));
-      }
+      cb(new gutil.PluginError('gulp-stylus', err));
     })
     .then(function(css){
-      if (css) {
-        file.path = rext(file.path, '.css');
-        file.contents = new Buffer(css);
-        that.push(file);
-      }
-      cb();
+      if(!css) return;
+      file.path = rext(file.path, '.css');
+      file.contents = css ? new Buffer(css) : null;
+      cb(null, file);
     });
   });
 
