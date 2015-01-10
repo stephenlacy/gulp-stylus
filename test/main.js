f7f12d49bec29e1e2e811223d50cddd74624dc63
Steve Lacy <me@slacy.me>
2015-01-10 13:47:31 -0700

    normalize fn brackets

--- test/main.js
+++ test/main.js
@@ -8,8 +8,8 @@ var fs = require('fs');
 
 require('mocha');
 
-describe('gulp-stylus', function(){
-	it('should render stylus .styl to CSS .css', function(done){
+describe('gulp-stylus', function() {
+	it('should render stylus .styl to CSS .css', function(done) {
 		var stream = stylus();
 
 		var fakeFile = new gutil.File({
@@ -19,7 +19,7 @@ describe('gulp-stylus', function(){
 			contents: fs.readFileSync('test/fixtures/normal.styl')
 		});
 
-		stream.once('data', function(newFile){
+		stream.once('data', function(newFile) {
 			should.exist(newFile);
 			should.exist(newFile.contents);
 			String(newFile.contents).should.equal(fs.readFileSync('test/expected/normal.css', 'utf8'));
@@ -30,7 +30,7 @@ describe('gulp-stylus', function(){
 
 	});
 
-	it ('should compress when called', function(done){
+	it ('should compress when called', function(done) {
 		var stream = stylus({compress: true});
 		var fakeFile = new gutil.File({
 			base: 'test/fixtures',
@@ -52,7 +52,7 @@ describe('gulp-stylus', function(){
 		stream.end();
 	});
 
-	it ('should import other .styl files', function(done){
+	it ('should import other .styl files', function(done) {
 		var stream = stylus({import: __dirname + '/fixtures/one.styl'});
 		var fakeFile = new gutil.File({
 			base: 'test/fixtures',
@@ -73,7 +73,7 @@ describe('gulp-stylus', function(){
 		stream.end();
 	});
 
-	it ('should define variables in .styl files', function(done){
+	it ('should define variables in .styl files', function(done) {
 		var stream = stylus({define: {'white': '#fff'}});
 		var fakeFile = new gutil.File({
 			base: 'test/fixtures',
@@ -94,7 +94,7 @@ describe('gulp-stylus', function(){
 		stream.end();
 	});
 
-	it('should skip css files', function(done){
+	it('should skip css files', function(done) {
 		var stream = stylus();
 
 		var fakeFile = new gutil.File({
@@ -104,7 +104,7 @@ describe('gulp-stylus', function(){
 			contents: fs.readFileSync('test/fixtures/ie8.css')
 		});
 
-		stream.once('data', function(newFile){
+		stream.once('data', function(newFile) {
 			should.exist(newFile);
 			should.exist(newFile.contents);
 			String(newFile.contents).should.equal(fs.readFileSync('test/fixtures/ie8.css', 'utf8'));
@@ -116,7 +116,7 @@ describe('gulp-stylus', function(){
 	});
 
 
-	it('should throw on parse error', function(done){
+	it('should throw on parse error', function(done) {
 		var stream = stylus();
 
 		var file = 'test/fixtures/error.styl';
@@ -138,7 +138,7 @@ describe('gulp-stylus', function(){
 
   });
 
-	it ('should import nested and reverse recursive files', function(done){
+	it ('should import nested and reverse recursive files', function(done) {
 		var stream = stylus();
 		var fakeFile = new gutil.File({
 			base: 'test/fixtures',
@@ -157,7 +157,7 @@ describe('gulp-stylus', function(){
 		stream.end();
 	});
 
-	it ('should generate sourcemaps', function(done){
+	it ('should generate sourcemaps', function(done) {
 		var stream = stylus({sourcemap: true});
 
 		var fakeFile = new gutil.File({
@@ -182,7 +182,7 @@ describe('gulp-stylus', function(){
 
 	});
 
-	it ('should generate sourcemaps with gulp-sourcemaps', function(done){
+	it ('should generate sourcemaps with gulp-sourcemaps', function(done) {
 		var stream = sourcemaps.init();
 
 		var fakeFile = new gutil.File({
